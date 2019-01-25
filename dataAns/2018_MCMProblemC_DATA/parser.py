85330584b9c6c368c5cd6a0f349e22b90ca38fd6
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-25 08:31:23 +0800

    提交药品列表。

--- dataAns/2018_MCMProblemC_DATA/parser.py
+++ dataAns/2018_MCMProblemC_DATA/parser.py
@@ -10,6 +10,9 @@ from structor import County, Record
 readfilename = input("Input [filename].pkl... \n>>> ")
 rq = []
 
+if readfilename == "":
+    readfilename = "cnt_db"
+
 with open("%s.pkl" % readfilename, 'rb') as file:
     rq = pickle.loads(file.read())
 
@@ -23,6 +26,10 @@ rcd_data = pd.read_csv('MCM_NFLIS_Data.csv')
 
 data_len = len(rcd_data)
 
+drug_type = set()
+
 for rec in range(data_len):
-    print(rcd_data.iloc[rec])
-    input()
+    rec_item = rcd_data.iloc[rec]
+    drug_type.add(rec_item['SubstanceName'])
+
+print(drug_type)

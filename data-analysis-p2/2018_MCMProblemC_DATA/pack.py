93c1afed46b2f9fddfba3dcc47633f15eaf4aacf
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-27 13:22:50 +0800

    准备问题二数据获取。

---
+++ data-analysis-p2/2018_MCMProblemC_DATA/pack.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import numpy as np
+from math import sqrt, e, sin
+
+
+class Pack:
+    id = 0
+    name = ""
+    x = 0.0
+    y = 0.0
+    nearest_id = []
+    history = []
+    estm_history = []
+    w = 0.0
+    p = 0.0
+    affect_index_a = 0.0
+    affect_index_b = 0.0
+    affect_index_c = 0.0
+    affect_index_d = 0.0
+    affect_index_e = 0.0
+
+    def get_interior_index(self, year):
+        return np.mean(self.history) * pow(e, sin(self.p + year * self.w))
+
+    def get_distance(self, lib):
+        dists = []
+        for i in self.nearest_id:
+            dists.append(
+                sqrt((self.x - lib[i].x) ** 2 + (self.y - lib[i].y) ** 2))
+        return dists
+
+    def __init__(self, id, x, y, name, nearest_id=[]):
+        self.x = x
+        self.y = y
+        self.id = id
+        self.name = name
+        self.nearest_id = nearest_id
+        self.history = [0] * 8
+
+    def __str__(self):
+        return """ #%d
+        City [%s] at [%.4f, %.4f]
+        Nearest to: %s
+        w = %.4f, p = %.4f, affect_index = %.4f, %.4f, %.4f, %.4f, %.4f
+        """ % (self.id, self.name, self.x, self.y, self.nearest_id, self.w, self.p, self.affect_index_a, self.affect_index_b, self.affect_index_c, self.affect_index_d, self.affect_index_e)
+
+    def print(self, lib):
+        print(""" #%d
+        City [%s] at [%.4f, %.4f]
+        History: %s
+              """ % (self.id, self.name, self.x, self.y, ' -> '.join(str(v) for v in self.history)))
+
+        for i in self.nearest_id:
+            dist = sqrt((self.x - lib[i].x) ** 2 + (self.y - lib[i].y) ** 2)
+
+            print("""
+                Closest to: #%d, City [%s] at [%.4f, %.4f], distance = %.3f
+                History: %s
+            """ % (i, lib[i].name, lib[i].x, lib[i].y, dist, ' -> '.join(str(v) for v in lib[i].history)))
+
+    def get_mma_script(self, lib):
+        result = ""
+        result += ("Avg = %d\n" % (int(np.mean(self.history))))
+
+        result += ("dt = {%s}\n" % (', '.join(str(v) for v in self.history)))
+
+        count = 1
+        for i in self.nearest_id:
+            result += ("D%d = %.4f\n" % (count,
+                                         sqrt((self.x - lib[i].x) ** 2 + (self.y - lib[i].y) ** 2)))
+            count += 1
+
+        count = 1
+        for i in self.nearest_id:
+            result += ("P%d = {%s}\n" % (count, ', '.join(str(v)
+                                                          for v in lib[i].history)))
+
+            count += 1
+
+        return result
+
+    def print_for_fitting(self, lib):
+
+        print("Avg = %d" % (int(np.mean(self.history))))
+
+        print("dt = {%s}" % (', '.join(str(v) for v in self.history)))
+
+        count = 1
+        for i in self.nearest_id:
+            print("D%d = %.4f" % (count,
+                                  sqrt((self.x - lib[i].x) ** 2 + (self.y - lib[i].y) ** 2)))
+            count += 1
+
+        count = 1
+        for i in self.nearest_id:
+            print("P%d = {%s}" % (count, ', '.join(str(v)
+                                                   for v in lib[i].history)))
+
+            count += 1
---
+++ data-analysis-p2/2018_MCMProblemC_DATA/weight_gen.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import csv
+import pickle
+import numpy as np
+import pandas as pd
+from pack import Pack
+
+target_params = ['HC01_VC03']
+
+
+def get_filehead(yr):
+    return "ACS_%d_5YR_DP02/ACS_%d_5YR_DP02_with_ann.csv" % (
+        yr + 10, yr + 10)
+
+
+readfilename = input("Input [filename].obj... \n>>> ")
+packages = []
+
+if readfilename == "":
+    readfilename = "raw_model"
+
+with open("%s.obj" % readfilename, 'rb') as file:
+    packages = pickle.loads(file.read())
+
+
+if type(packages) != list:
+    raise TypeError("Bad File Format")
+
+print("Successfully get %d items." % len(packages))
+
+results = []
+
+for year in range(7):
+    print("=== Processing year %d ===" % (year + 2010))
+    csv_file = get_filehead(year)
+    fs = pd.read_csv(csv_file)
+    size = len(fs)
+    for pk in packages:
+        print(pk)
+        internal_index = pk.get_interior_index(year)
+        params = [year + 2010, pk.id, pk.name, internal_index]
+        fine = False
+        for i in range(size):
+            itm = fs.iloc[i]
+            if itm['GEO.display-label'].replace(', ', '/').replace(' County', '').replace(' city', '') == pk.name.replace(' (city)', ''):
+                for p in target_params:
+                    params.append(itm[p])
+                fine = True
+                break
+        if not fine:
+            for p in target_params:
+                params.append(0)
+                input("No data found for #%d %s." % (pk.id, pk.name))
+        print(params)
+        results.append(params)
+
+savefilename = input("Save it to [where].csv... \n>>> ")
+
+if savefilename == "":
+    savefilename = "weight"
+
+savefilename += '.csv'
+
+with open(savefilename, 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile)
+    spamwriter.writerow(
+        ["Year", "ID", "Name", "InnerIndex"] + target_params)
+    for row in results:
+        spamwriter.writerow(row)

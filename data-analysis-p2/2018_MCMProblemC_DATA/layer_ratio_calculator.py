7cf526cda381bbf641be7642c87af29a8b0afa61
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-27 20:23:41 +0800

    为层析法做准备。

---
+++ data-analysis-p2/2018_MCMProblemC_DATA/layer_ratio_calculator.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import csv
+import pickle
+import numpy as np
+import pandas as pd
+from pack import Pack
+
+
+readfilename = input("Input [filename].csv... \n>>> ")
+packages = []
+
+if readfilename == "":
+    readfilename = "cherish"
+
+
+description = ['Family * a + Education',
+               'Education * b + Culture', 'Family * c + Culture']
+
+words = ['a', 'b', 'c']
+
+
+weight_csv = pd.read_csv(readfilename + ".csv")
+length = len(weight_csv)
+
+print("Successfully parsed %d items in csv file." % length)
+
+Index_a = 0.0
+Index_b = 0.0
+Index_c = 0.0
+
+total_weight = 0
+
+for index in range(length):
+    loc_x = weight_csv.iloc[index]
+    weight = int(loc_x['Weight'])
+    total_weight += weight
+    type = int(loc_x['Type'])
+    if type == 0:
+        Index_a += float(loc_x['x value']) * weight
+    elif type == 1:
+        Index_b += float(loc_x['x value']) * weight
+    else:
+        Index_c += float(loc_x['x value']) * weight
+
+
+total_weight /= 3.0
+
+Index_a /= total_weight
+Index_b /= total_weight
+Index_c /= total_weight
+
+result = [Index_a, Index_b, Index_c]
+
+for i in range(3):
+    print("""
+Result:
+    [%s]::%s = %.5f
+
+""" % (description[i], words[i], result[i]))
